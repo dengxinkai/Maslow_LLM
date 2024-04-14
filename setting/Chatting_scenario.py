56b7e0800c1b5c668f1c51295e4c50f825ca990c
XinKai <78074644+dengxinkai@users.noreply.github.com>
2024-04-14 16:51:19 +0800

    Create Chatting_scenario.py

---
+++ setting/Chatting_scenario.py
@@ -0,0 +1,10 @@
+#参与者有关的客观因素
+time = "晚上7点，"
+place = "某个酒吧，"
+medium = "偶遇，非正式的当面聊天"
+occasion = time + place + medium#谈话场景
+score = 20
+role = "郑总" #谈话对象
+task = f"销售一款理财产品给{role}" #挑战
+relationship = "你们的关系非常好" #关系
+decide = "是否购买理财产品"
