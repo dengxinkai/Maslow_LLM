317689d933f1757e7930dd2235098f73f19a814b
XinKai <78074644+dengxinkai@users.noreply.github.com>
2024-04-16 14:44:22 +0800

    Update Chatting_ending.py

--- ending/Chatting_ending.py
+++ ending/Chatting_ending.py
@@ -1,11 +1,11 @@
 def ending(ww):
     a = ""
     completion = client.chat.completions.create(
-        model="gpt-4-0125-preview",
+        model="gpt-4-turbo-2024-04-09",
         messages=[
             {"role": "system", "content": f"""
-            对话记录以及满意程度为：{ww}
-            直到满意程度超过49，游戏通关，满意程度低于或等于0则游戏失败。
+            对话记录、满意程度以及双方关系变化如下：{ww}
+            直到满意程度超过49，游戏通关，满意程度低于或等于0则谈话失败。
             如果通关，根据上面的对话总结出谈话成功的经验，如果失败，根据上面的对话总结出谈话失败的教训"""}
         ],
         stream=True
